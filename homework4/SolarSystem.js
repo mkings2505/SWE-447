24d243e7e566597cca92a478c149231931aeff3e
mkings2505 <mkings2505@yahoo.com>
2016-04-05 19:32:52 -0700

    Create SolarSystem.js

---
+++ homework4/SolarSystem.js
@@ -0,0 +1,57 @@
+var SolarSystem = {
+  Sun : {
+    radius : 10*3*.75,
+    distance : 0,
+    color : [ 1.0, 1.0, 0.0, 1.0 ]
+  },
+  Mercury : {
+    radius : 0.0553*3*.75,
+    distance : 0.387,
+    color : [ 1.0, 0.0, 0.0, 1.0 ]
+  },
+  Venus : {
+    radius : 0.949*2.7*.75,
+    distance : 0.723,
+    color : [ 1.0, 0.0, 1.0, 1.0 ]
+  },
+  Earth : {
+    radius : 1*3*.75,
+    distance : 1,
+    color : [ 0.0, 0.0, 1.0, 1.0 ]
+  },
+  Moon : {
+    radius : 0.2724,
+    distance : 0.00257*40,
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+  Mars : {
+    radius : 0.532*2.5*.75,
+    distance : 1.52,
+    color : [ 1.0, 0.0, 0.0, 1.0 ]
+  },
+  Jupiter : {
+    radius : 11.21*.4*.75,
+    distance : 5.20*.3,
+    color : [ 1.0, 153/255, 0.0, 1.0 ]
+  },
+  Saturn : {
+    radius : 9.45*.25*.75,
+    distance : 9.58,
+    color : [ 1.0, 1.0, 0.0, 1.0 ]
+  },
+  Uranus : {
+    radius : 4.01*.5*.75,
+    distance : 19.20,
+    color : [ 0.0, 0.1, 1.0, 1.0 ]
+  },
+  Neptune : {
+    radius : 3.88*.5*.75,
+    distance : 30.05,
+    color : [ 0.0, 0.0, 1.0, 1.0 ]
+  },
+  Pluto : {
+    radius : 0.186*2,
+    distance : 39.48,
+    color : [ 150/255, 75/255, 0.0, 1.0 ]
+  }
+};
